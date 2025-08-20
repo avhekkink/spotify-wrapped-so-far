63d160c61bb2cf69aa13285aefc7fef3cde88901
AVHekkink <annavhekkink@gmail.com>
2025-08-20 12:48:42 +1000

    add suspense boundary when using useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { authorize } from './utils/authorization.ts';
@@ -12,24 +12,26 @@ function Home() {
   useRefreshToken(code as string);
 
   return (
-    <div className="w-full h-full flex-1 overflow-y-auto place-content-center">
-      <div className="w-full h-full rounded-lg flex flex-col place-content-center">
-        {!code ? (
-          <h1 className="text-white text-5xl py-8 self-center">
-            Connect your Spotify
-          </h1>
-        ) : null}
-        <div className="bg-blue rounded-full shadow w-80 self-center py-1 px-32 flex justify-center">
-          {code ? (
-            <Link href="/dashboard">Continue</Link>
-          ) : (
-            <button type="button" onClick={authorize} className="btn btn-accent font-bold">
-              LOG IN
-            </button>
-          )}
+    <Suspense fallback={<div className="w-full h-full flex-1 overflow-y-auto place-content-center"><h1 className="text-white text-5xl py-8 self-center">Loading...</h1></div>}>
+      <div className="w-full h-full flex-1 overflow-y-auto place-content-center">
+        <div className="w-full h-full rounded-lg flex flex-col place-content-center">
+          {!code ? (
+            <h1 className="text-white text-5xl py-8 self-center">
+              Connect your Spotify
+            </h1>
+          ) : null}
+          <div className="bg-blue rounded-full shadow w-80 self-center py-1 px-32 flex justify-center">
+            {code ? (
+              <Link href="/dashboard">Continue</Link>
+            ) : (
+              <button type="button" onClick={authorize} className="btn btn-accent font-bold">
+                LOG IN
+              </button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
